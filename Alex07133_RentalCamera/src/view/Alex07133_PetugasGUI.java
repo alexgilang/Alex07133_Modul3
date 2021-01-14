6e9005709a1a1f5da83c3b540c34ca1a815a932e
alee <alexgilangs12@gmail.com>
2021-01-14 20:31:28 +0700

    Initial commit

---
+++ Alex07133_RentalCamera/src/view/Alex07133_PetugasGUI.java
@@ -0,0 +1,19 @@
+
+package view;
+import Alex07133_Controller.Alex07133_AllObjectModel;
+import Alex07133_Controller.Alex07133_PetugasController;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.*;
+
+public class Alex07133_PetugasGUI {
+    private static Alex07133_PetugasController admin = new Alex07133_PetugasController();
+    JFrame Admin = new JFrame();
+    JTable tabelpetugas = new JTable();
+    
+    
+}
---
+++ Alex07133_RentalCamera/src/view/allobjct07133.java
@@ -0,0 +1,9 @@
+
+package view;
+import Alex07133_Controller.Alex07133_PetugasController;
+import Alex07133_Controller.Alex07133_PelangganController;
+
+public class allobjct07133 {
+    public static Alex07133_PetugasController admin = new Alex07133_PetugasController();
+    public static Alex07133_PelangganController pelanggan = new Alex07133_PelangganController();
+}
